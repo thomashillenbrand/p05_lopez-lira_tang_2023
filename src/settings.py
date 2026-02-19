f1414a5248e4c52d971d4226f1a780cb30049418
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2026-02-18 19:13:54 -0600

    update env vars, requirements, and settings.py for project

--- src/settings.py
+++ src/settings.py
@@ -148,7 +148,7 @@ else:
     defaults["STATA_EXE"] = get_stata_exe()
 
 ## Dates
-defaults["START_DATE"] = datetime.strptime("1913-01-01", "%Y-%m-%d")
+defaults["START_DATE"] = datetime.strptime("2021-10-01", "%Y-%m-%d")
 defaults["END_DATE"] = datetime.strptime("2024-12-31", "%Y-%m-%d")
 
 
